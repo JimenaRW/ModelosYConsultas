118de5b14683594434e48a098be8af9c95c73f5c
jimenarw <jimena.witen@gmail.com>
2021-09-26 22:22:01 -0300

    configurando tablename

--- src/database/models/Genre.js
+++ src/database/models/Genre.js
@@ -1,38 +1,41 @@
-module.exports = (sequelize,dataTypes) => {
+module.exports = (sequelize, dataTypes) => {
 
-    const alias = "Genero";
+  const alias = 'Genero';
 
-    const  cols = {
+  const cols = {
 
-        id : {
-            type : dataTypes.INTEGER.UNSIGNED,
-            primaryKey : true,
-            allowNull : false,
-            autoIncrement : true
-        },
-        name : {
-            type : dataTypes.STRING(100),
-            allowNull : false
-        },
-        ranking : {
-            type : dataTypes.INTEGER.UNSIGNED,
-            allowNull : false,
-            unique : true
-        },
-        active : {
-            type: dataTypes.BOOLEAN,
-            defaultValue : 1,
-            allowNull : false
-        }
-
+    id: {
+      type: dataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    name: {
+      type: dataTypes.STRING(100),
+      allowNull: false
+    },
+    ranking: {
+      type: dataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      unique: true
+    },
+    active: {
+      type: dataTypes.BOOLEAN,
+      defaultValue: 1,
+      allowNull: false
     }
 
-    const config = {
-        underscored : true
-    }
+  }
+
+  const config = {
+    tableName : 'genres',
+    timestamps : true,
+    underscored: true
+  }
 
 
-    const Genre = sequelize.define(alias, cols, config)
+  const Genre = sequelize.define(alias, cols, config)
 
-    return Genre
+  return Genre
+
 }
