2583c06668c5eac520db22d30a085210f4957fab
jimenarw <jimena.witen@gmail.com>
2021-09-28 00:13:34 -0300

    CRUD con validaciones

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,5 +1,7 @@
 const db = require('../database/models');
 
+const { validationResult } = require('express-validator');
+
 
 module.exports = {
 
@@ -8,42 +10,77 @@ module.exports = {
     },
 
     create: (req, res) => {
-        const { title } = req.body;
 
-        db.Pelicula.create({
-            ...req.body,
-            title: title.trim()
-        })
-            .then(movie => {
-                console.log(movie)
-                res.redirect('/movies/detail/' + movie.id)
+        let errors = validationResult(req)
+
+        if (errors.isEmpty()) {
+
+            const { title } = req.body;
+
+            db.Pelicula.create({
+                ...req.body,
+                title: title.trim()
             })
+                .then(movie => {
+                    console.log(movie)
+                    res.redirect('/movies/detail/' + movie.id)
+                })
+                .catch(e => console.log(e))
+        }
+        else {
+            return res.render('moviesAdd', {
+                errores: errors.mapped(),
+                old: req.body
+            })
+        }
+        
+        
     },
 
     edit: (req, res) => {
         db.Pelicula.findByPk(req.params.id)
-            .then(Movie => res.render('moviesEdit', {
-                Movie
-            }))
+        .then(Movie => {
+            return res.send(Movie.release_date)
+            res.render('moviesEdit', {
+                Movie,
+                release_date
+
+            })
+        }) 
 
     },
 
     update: (req, res) => {
-        db.Pelicula.update(
-            {
-                ...req.body
-            },
-            {
-                where: {
-                    id: req.params.id
-                }
-            }
-        )
+        let errors = validationResult(req)
 
-            .then(() => {
-                res.redirect('/movies/detail/' + req.params.id)
-            })
+        if (errors.isEmpty()) {
+            const { title } = req.body;
+            db.Pelicula.update(
+                {
+                    ...req.body,
+                    title: title.trim()
+                },
+                {
+                    where: {
+                        id: req.params.id
+                    }
+                }
+            )
+
+                .then(() => {
+                    res.redirect('/movies/detail/' + req.params.id)
+                })
+                .catch(e => console.log(e))
+        }
+        else {
+            db.Pelicula.findByPk(req.params.id)
+            .then(Movie => res.render('moviesEdit', {
+                Movie,
+                errores: errors.mapped(),
+                old: req.body
+            }))
             .catch(e => console.log(e))
+        }
 
     },
 
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { add, create, edit, update, remove, destroy } = require('../controllers/adminController');
 
+/* Validations */
+const moviesValidator = require('../validations/moviesValidator');
 
 /* routing de /admin*/
 router
     .get('/create', add)
-    .post('/create', create)
+    .post('/create', moviesValidator, create)
     .get('/edit/:id', edit)
-    .put('/update/:id', update)
+    .put('/update/:id', moviesValidator, update)
     .get('/destroy/:id', remove)
     .delete('/delete/:id', destroy)
 
